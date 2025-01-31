9e5dbe0c49408463a5285322f1c2d281db1311d2
杨逸夫 <yangyifu@yangyifudeMBP.station>
2025-01-31 11:47:21 +0100

    Update website style and add image slider

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example of a simple smooth scroll effect
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
